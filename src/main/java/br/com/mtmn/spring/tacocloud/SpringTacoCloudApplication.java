a0b804eb6c82f5b7f40fb0b74133b234f94a86d2
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2019-07-27 08:30:08 -0300

    Initial commit

---
+++ src/main/java/br/com/mtmn/spring/tacocloud/SpringTacoCloudApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtmn.spring.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTacoCloudApplication.class, args);
+	}
+
+}
