5cb687c683770e0fd0c3bd0094fd82a9acf59b14
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2019-07-28 12:19:57 -0300

    Implementing form validation.

--- src/main/java/br/com/mtmn/spring/tacocloud/web/DesignTacoController.java
+++ src/main/java/br/com/mtmn/spring/tacocloud/web/DesignTacoController.java
@@ -3,17 +3,21 @@ package br.com.mtmn.spring.tacocloud.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import br.com.mtmn.spring.tacocloud.domain.Ingredient;
 import br.com.mtmn.spring.tacocloud.domain.Ingredient.Type;
 import br.com.mtmn.spring.tacocloud.domain.Taco;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -35,13 +39,11 @@ public class DesignTacoController {
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
 
-        Type[] types = Type.values();
-        for (Type type : types) {
+        Stream.of(Type.values()).forEach(type -> {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
-        }
+        });
 
         model.addAttribute("design", new Taco());
-
         return "design";
     }
 
@@ -56,7 +58,6 @@ public class DesignTacoController {
         // Save the taco design...
         // We'll do this in chapter 3
         log.info("Processing design: " + design);
-
         return "redirect:/orders/current";
     }
 }
